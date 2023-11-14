bb51cd5304dc203274599af4a2ad2d05b7f73931
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-11-14 22:25:12 +0530

    Add files via upload

---
+++ diffoperationsonnumbers.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value a"));
+b=int(input("enter the value b"));
+print("the addition of a and b is", a+b);
+print("the subraction of a and b is", a-b);
+print("the multiplication of a and b is", a*b);
+print("the division of a and b is", a/b);
+print("the remainder of a and b is", a%b);
+print("the exponent of a and b is", a**b);
+print("the floordivision of a and b is", a//b);
