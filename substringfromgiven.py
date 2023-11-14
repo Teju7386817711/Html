f1f2f734d4e868b6cb45274dad24fee8f26178d2
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-11-14 22:41:16 +0530

    Add files via upload

---
+++ substringfromgiven.py
@@ -0,0 +1,6 @@
+string1=input("enter the string:");
+string2=input("enter the second string:");
+print("first string:", string1);
+print("second string:", string2);
+print("concatanation of two strings:", string1+string2);
+print("substring to given string:", string1[1:3]);
