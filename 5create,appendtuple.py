034f4e5dc9e938d1f198c3cd4c93ed2eaa579a1f
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-11-15 17:03:05 +0530

    Add files via upload

---
+++ 5create,appendtuple.py
@@ -0,0 +1,11 @@
+print("Hello world")
+a=("apple","banana","cherry","mango","grape","orange")
+print("\n created tuple is:",a)
+print("\n second fruit is:",a[1])
+print("\n from 3-6 fruits are:",a[3:6])
+print("\n list of all items in tuple is:")
+for x in a:
+    print(x)
+if"apple" in a:
+    print("\n yes,apple is in fruits tuple")
+print("\n lenghth of tuple is:",len(a))
