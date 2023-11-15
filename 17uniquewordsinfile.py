127842a21f84ec2b3ad0f0d7481df2a7e3e67c61
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-11-15 21:03:58 +0530

    Add files via upload

---
+++ 17uniquewordsinfile.py
@@ -0,0 +1,15 @@
+filename=input("Enter file name : ")
+fh=open(filename)
+lst=list()
+words=[]
+for line in fh:
+    words+=line.split()
+words.sort()
+print("The unique words in aplhabetical order are : ")
+for word in words:
+    if word in lst:
+        continue
+    else:
+        lst.append((word))
+        print(word)
+print(lst)
