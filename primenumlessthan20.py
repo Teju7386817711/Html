266aa8338ff6317c40c2307b3ee74bc90b8bc4d3
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-11-15 12:07:50 +0530

    Add files via upload

---
+++ primenumlessthan20.py
@@ -0,0 +1,10 @@
+Starting_value = 1
+n = 20
+print("Prime numbers between",Starting_value,"and",n,"are:")
+for num in range(Starting_value,n+1):
+    if num > 1:
+        for i in range(2,int(num/2)+1):
+            if(num%i)==0:
+                break
+        else:
+            print(num)
