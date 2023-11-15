4267f258b7f798b4031e2ef3a5ac0d9879fe6fc1
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-11-15 12:45:04 +0530

    Add files via upload

---
+++ importfuntoanother.py
@@ -0,0 +1,6 @@
+from arth import Add
+from arth import Sub
+num1=float(input("enter the first number:"))
+num2=float(input("enter the second number:"))
+print("addition of two numbers is:", Add(num1,num2))
+print("subraction of two numbers is:", Sub(num1,num2))
