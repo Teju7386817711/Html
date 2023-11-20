1cd6651a7fc14dd02fa45c98e946251acc4120d6
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-11-20 14:46:14 +0530

    Add files via upload

---
+++ 20nov.js
@@ -0,0 +1,10 @@
+const num = prompt('Enter a positive number: ');
+
+console.log(`The factors of ${num} is:`);
+for(let i = 1; i <= num; i++) {
+
+    // check if number is a factor
+    if(num % i == 0) {
+        console.log(i);
+    }
+}
