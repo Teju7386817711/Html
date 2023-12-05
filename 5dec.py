983f62dd9e55f27f256a7560f59d19d1ce71cd9e
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-12-05 20:26:37 +0530

    Add files via upload

---
+++ 5dec.py
@@ -0,0 +1,13 @@
+# Adding Tuple to List
+
+test_list = [5, 6, 7]
+
+print("The original list is : " + str(test_list))
+
+
+test_tup = (9, 10) # initializing tuple 
+
+test_list += test_tup
+
+# printing result 
+print(" after addition : " + str(test_list)) 
