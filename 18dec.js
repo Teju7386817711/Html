5a2826bd82a9206c6ca4db18cf103eb5d7e7d025
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2023-12-18 14:02:40 +0530

    Add files via upload

---
+++ 18dec.js
@@ -0,0 +1,12 @@
+function addElement(arr) {
+
+    // adding new array element
+    arr.unshift(4);
+    
+    console.log(arr);
+}
+
+const array = [1, 2, 3];
+
+// calling the function and passing array argument
+addElement(array);
