0acc0026a8d7cb93ef26150cd765c468d05a8b07
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2024-01-02 11:11:26 +0530

    Add files via upload

---
+++ 2jan.js
@@ -0,0 +1,14 @@
+const string = prompt('Enter a sentence: ');
+
+// converting to an array
+const words = string.split(' ');
+
+// sort the array elements
+words.sort();
+
+// display the sorted words
+console.log('The sorted words are:');
+
+for (const element of words) {
+  console.log(element);
+}
