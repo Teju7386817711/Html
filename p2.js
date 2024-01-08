2ab30401c2d6e6a5a0ef8adfd3388e0bdac67075
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2024-01-08 14:53:35 +0530

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,39 @@
+//without arguements
+function name(){
+    console.log("hello");
+}
+name();
+
+
+
+//with arguements
+// function addNums(num1,num2)
+// {
+//     return(num1+num2);
+// }
+// addNums(10,20);
+//global variable
+// let a=10;
+// function myFun(){
+//     console.log(a);
+// }
+
+
+// //global scope
+// let a=10;
+// function myFun(){
+//      console.log(a);
+// }
+// myFun();
+// console.log(a);
+
+
+
+
+// //local scope
+// function myFun(){
+//     let a=10;
+//    console.log(a);
+// }
+// myFun();
+// console.log(a);
