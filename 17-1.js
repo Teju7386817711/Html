02d9bf1618aef3ead4a315c263f218e28f60d154
Teju7386817711 <143092027+Teju7386817711@users.noreply.github.com>
2024-02-02 11:17:59 +0530

    Add files via upload

---
+++ 17-1.js
@@ -0,0 +1,10 @@
+const array = ['this', 'is', 'teja'];
+
+const hasValue = array.includes('teja');
+
+// checking the condition
+if(hasValue) {
+    console.log('Array contains a value.');
+} else {
+    console.log('Array does not contain a value.');
+}
---
+++ 18-1.js
@@ -0,0 +1,28 @@
+// program to write to console
+
+
+console.log(8);// passing number 
+
+
+console.log('hello');// passing string
+
+
+const x = 'hello';// passing variable
+console.log(x);
+
+
+function sayName() {// passing function
+    return 'Hello tej';
+}
+console.log(sayName());
+
+
+const name = 'tej'; // passing string and a variable
+console.log('Hello ' + name);
+
+
+let obj = {// passing object
+    name: 'tej',
+    age: 28
+}
+console.log(obj);
